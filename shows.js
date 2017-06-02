08d179c9ad9e9e157cbaa889b65e8eb421f0e365
Kevin McSorley <kmcsorley@srcs.loc>
2017-06-02 13:44:50 -0700

    added shows

---
+++ shows.js
@@ -0,0 +1,132 @@
+/**
+ * Uses AJAX to query an internet data source for the id number given a show title
+ * @param {string} showId The element id that has the show title
+ */
+function getId(showId) {
+
+    // First get the name of the show from the HTML dropdown selector code
+    var shows = document.getElementsByName(showId);
+    var show_name = "";
+    for(i = 0; i < shows.length; i++){
+        if(shows[i].selected){
+            show_name = shows[i].value;
+        }
+    }
+    console.log(show_name);
+
+    // Now make a HTTP request
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            // We got a response from the server!
+            if(this.status === 200) {
+                // The request was successful!
+                getCast(this.responseText);
+            } else if (this.status === 404){
+                // No postal code found
+                getCast('{ "id" : "none" }');
+            } else {
+                console.log("We have a problem...server responded with code: " + this.status);
+            }
+        } else {
+            // Waiting for a response...
+        }
+    };
+    // Notice how the URL is appended with the show name
+    var url = "http://api.tvmaze.com/search/shows?q=" + show_name; //"http://api.tvmaze.com/shows/:id/cast"+show;
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+}
+
+function getCast(showData){
+    var showInfo = JSON.parse(showData);
+    var id = showInfo[0].show.id;
+    console.log(id);
+    // Now make a HTTP request
+    var httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = function () {
+        if (this.readyState === 4) {
+            // We got a response from the server!
+            if(this.status === 200) {
+                // The request was successful!
+                displayCast(this.responseText);
+            } else if (this.status === 404){
+                // No postal code found
+                displayCast('{ "country" : "none" }');
+            } else {
+                console.log("We have a problem...server responded with code: " + this.status);
+            }
+        } else {
+            // Waiting for a response...
+        }
+    };
+    // Notice how the URL is appended with the show name
+    var url = "http://api.tvmaze.com/shows/" + id + "/cast";
+    httpRequest.open("GET", url, true);
+    httpRequest.send();
+}
+
+
+/**
+ * Displays the zip code place given the JSON data
+ * @param {string} data JSON data representing place for given zip code
+ */
+function displayCast(data){
+    var cast = JSON.parse(data);
+    console.log(cast[0].person.name);
+    console.log(cast[0].person.image.medium);
+    if(cast[0].person.name === "none") {
+        document.getElementById("cast").className = "alert alert-warning";
+        document.getElementById("cast").innerHTML = "No cast matches the show."
+    } else {
+
+        var display = "<table> <tr> <th>PHOTO</th><th>ACTOR</th><th>CHARACTER</th></tr>";
+
+        for(i = 0; i < cast.length; i++){
+            display = display + "<tr><td>"+getImage(cast[i].person.image.medium)+"</td><td>"+cast[i].person.name+"</td><td>"+cast[i].character.name+"</td></tr>";
+        }
+        display = display + "</table>";
+        /*
+        for(i = 0; i < cast.length; i++){
+            display = display + getImage(cast[i].person.image.medium) + "Actor: " + cast[i].person.name + "       Character: " + cast[i].character.name + "<hr>";
+        }
+        */
+
+        //style images after they are created
+        document.getElementById("cast").innerHTML = display;
+        var images = document.getElementsByTagName("img");
+        for(i = 0; i < images.length; i++){
+            images[i].style.width = "60px";
+            images[i].style.margin = "5px 20px 5px 5px";
+        }
+
+        //style table rows after they are created
+        var rows = document.getElementsByTagName("tr");
+        for(i = 0; i < rows.length; i++){
+            rows[i].style.border = "1px solid grey";
+        }
+
+        //style table cells after they are created
+        var cells = document.getElementsByTagName("td")
+        for(i = 0; i < cells.length; i++){
+            cells[i].style.padding = "0 5px 0 5px";
+            cells[i].style.fontSize = "16px";
+            cells[i].style.backgroundColor = "whitesmoke";
+        }
+
+        var header = document.getElementsByTagName("th");
+        for(i = 0; i < header.length; i++){
+            header[i].style.color = "white";
+            header[i].style.backgroundColor = "lightgrey";
+            header[i].style.fontSize = "20px";
+            header[i].style.padding = "0 5px 0 5px";
+        }
+
+    }
+}
+
+function getImage(source){
+    //source = "source";
+    var msg = "<img src=" + source + ">";
+    return msg;
+}

37e87d43dcd57402840a09abee2a96af64129260
Kevin McSorley <kmcsorley@srcs.loc>
2017-06-07 12:01:08 -0700

    commit

--- shows.js
+++ shows.js
@@ -7,8 +7,8 @@ function getId(showId) {
     // First get the name of the show from the HTML dropdown selector code
     var shows = document.getElementsByName(showId);
     var show_name = "";
-    for(i = 0; i < shows.length; i++){
-        if(shows[i].selected){
+    for (i = 0; i < shows.length; i++) {
+        if (shows[i].selected) {
             show_name = shows[i].value;
         }
     }
@@ -19,10 +19,10 @@ function getId(showId) {
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
-            if(this.status === 200) {
+            if (this.status === 200) {
                 // The request was successful!
                 getCast(this.responseText);
-            } else if (this.status === 404){
+            } else if (this.status === 404) {
                 // No postal code found
                 getCast('{ "id" : "none" }');
             } else {
@@ -38,7 +38,7 @@ function getId(showId) {
     httpRequest.send();
 }
 
-function getCast(showData){
+function getCast(showData) {
     var showInfo = JSON.parse(showData);
     var id = showInfo[0].show.id;
     console.log(id);
@@ -47,10 +47,10 @@ function getCast(showData){
     httpRequest.onreadystatechange = function () {
         if (this.readyState === 4) {
             // We got a response from the server!
-            if(this.status === 200) {
+            if (this.status === 200) {
                 // The request was successful!
                 displayCast(this.responseText);
-            } else if (this.status === 404){
+            } else if (this.status === 404) {
                 // No postal code found
                 displayCast('{ "country" : "none" }');
             } else {
@@ -71,61 +71,69 @@ function getCast(showData){
  * Displays the zip code place given the JSON data
  * @param {string} data JSON data representing place for given zip code
  */
-function displayCast(data){
+function displayCast(data) {
     var cast = JSON.parse(data);
     console.log(cast[0].person.name);
     console.log(cast[0].person.image.medium);
-    if(cast[0].person.name === "none") {
+    if (cast[0].person.name === "none") {
         document.getElementById("cast").className = "alert alert-warning";
         document.getElementById("cast").innerHTML = "No cast matches the show."
     } else {
 
         var display = "<table> <tr> <th>PHOTO</th><th>ACTOR</th><th>CHARACTER</th></tr>";
 
-        for(i = 0; i < cast.length; i++){
-            display = display + "<tr><td>"+getImage(cast[i].person.image.medium)+"</td><td>"+cast[i].person.name+"</td><td>"+cast[i].character.name+"</td></tr>";
+        for (i = 0; i < cast.length; i++) {
+            display += "<tr><td>" + getImage(cast[i].person.image.medium) + "</td><td>" + cast[i].person.name + "</td><td>" + cast[i].character.name + "</td></tr>";
         }
+
         display = display + "</table>";
-        /*
-        for(i = 0; i < cast.length; i++){
-            display = display + getImage(cast[i].person.image.medium) + "Actor: " + cast[i].person.name + "       Character: " + cast[i].character.name + "<hr>";
-        }
-        */
-
-        //style images after they are created
         document.getElementById("cast").innerHTML = display;
-        var images = document.getElementsByTagName("img");
-        for(i = 0; i < images.length; i++){
-            images[i].style.width = "60px";
-            images[i].style.margin = "5px 20px 5px 5px";
-        }
-
-        //style table rows after they are created
-        var rows = document.getElementsByTagName("tr");
-        for(i = 0; i < rows.length; i++){
-            rows[i].style.border = "1px solid grey";
-        }
-
-        //style table cells after they are created
-        var cells = document.getElementsByTagName("td")
-        for(i = 0; i < cells.length; i++){
-            cells[i].style.padding = "0 5px 0 5px";
-            cells[i].style.fontSize = "16px";
-            cells[i].style.backgroundColor = "whitesmoke";
-        }
-
-        var header = document.getElementsByTagName("th");
-        for(i = 0; i < header.length; i++){
-            header[i].style.color = "white";
-            header[i].style.backgroundColor = "lightgrey";
-            header[i].style.fontSize = "20px";
-            header[i].style.padding = "0 5px 0 5px";
-        }
-
+        styleDisplay();
     }
+
 }
 
-function getImage(source){
+
+
+function styleDisplay() {
+    //style images
+    var images = document.getElementsByTagName("img");
+    for (i = 0; i < images.length; i++) {
+        images[i].style.width = "60px";
+        images[i].style.margin = "5px 20px 5px 5px";
+    }
+
+    //style table rows
+    var rows = document.getElementsByTagName("tr");
+    for (i = 0; i < rows.length; i++) {
+        if (i % 2 == 0) {
+            rows[i].style.backgroundColor = "rgba(50, 50, 50, 0.1)";
+        } else {
+            rows[i].style.backgroundColor = "rgba(240, 240, 240, 0.1)";
+        }
+        rows[i].style.border = "1px solid grey";
+    }
+
+    //style table cells after they are created
+    var cells = document.getElementsByTagName("td")
+    for (i = 0; i < cells.length; i++) {
+        cells[i].style.padding = "0 5px 0 5px";
+        cells[i].style.fontSize = "16px";
+    }
+
+    //style header
+    var header = document.getElementsByTagName("th");
+    for (i = 0; i < header.length; i++) {
+        header[i].style.color = "white";
+        header[i].style.backgroundColor = "darkgrey";
+        header[i].style.fontSize = "20px";
+        header[i].style.fontWeight = "bold";
+        header[i].style.padding = "0 5px 0 5px";
+    }
+
+}
+
+function getImage(source) {
     //source = "source";
     var msg = "<img src=" + source + ">";
     return msg;
